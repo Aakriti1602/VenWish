c9f42d3da914318ff3a1a5f1657f08dc4c0ebc3c
Tanisha Bisht <agamyatani@gmail.com>
2021-03-21 08:35:31 +0530

    final changes made

--- frontendNew/index.js
+++ frontendNew/index.js
@@ -26,7 +26,7 @@ function storeWish() {
 const wish_div = document.querySelector('#wishesCards')
 db.collection('MakeAWish').get().then(function(querySnapshot){
 	querySnapshot.forEach(function(doc){
-		wish_div.innerHTML += '<div class="card"><div class="card-body"><h5 class="card-title">'+ doc.data().name +'</h5><p class="card-text">'+ doc.data().desc +'</p><p class="card-text"><span style="font-weight: 500;">CONTACT INFO:</span>'+ doc.data().cont +'</p><p style="text-align: right;"><a href="#" class="cardBtn">GRANT</a><a href="'+ doc.data().map +'" class="cardBtn">MAP</a></p></div></div>'
+		wish_div.innerHTML += '<div class="card"><div class="card-body"><h5 class="card-title">'+ doc.data().name +'</h5><p class="card-text">'+ doc.data().desc +'</p><p class="card-text"><span style="font-weight: 500;">CONTACT INFO:</span>'+ doc.data().cont +'</p><p style="text-align: right;"><a href="#" class="cardBtn">WISH</a><a href="'+ doc.data().map +'" class="cardBtn">MAP</a></p></div></div>'
 	})
 })
 
